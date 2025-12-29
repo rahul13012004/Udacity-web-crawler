b92d79dea15c0ed6db1c3ac6aabc2d14a0a0aa9c
rahul13012004 <rahul.bobbadi@gmail.com>
2025-12-29 23:22:39 +0530

    Step 2 Crawler Output

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
@@ -1,13 +1,20 @@
 package com.udacity.webcrawler.json;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Objects;
 
 /**
  * Utility class to write a {@link CrawlResult} to file.
  */
 public final class CrawlResultWriter {
+
   private final CrawlResult result;
 
   /**
@@ -26,9 +33,18 @@ public final class CrawlResultWriter {
    * @param path the file path where the crawl result data should be written.
    */
   public void write(Path path) {
-    // This is here to get rid of the unused variable warning.
     Objects.requireNonNull(path);
-    // TODO: Fill in this method.
+
+    try (Writer writer = Files.newBufferedWriter(
+            path,
+            StandardOpenOption.CREATE,
+            StandardOpenOption.APPEND)) {
+
+      write(writer);
+
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to write crawl results", e);
+    }
   }
 
   /**
@@ -37,8 +53,16 @@ public final class CrawlResultWriter {
    * @param writer the destination where the crawl result data should be written.
    */
   public void write(Writer writer) {
-    // This is here to get rid of the unused variable warning.
     Objects.requireNonNull(writer);
-    // TODO: Fill in this method.
+
+    try {
+      ObjectMapper mapper = new ObjectMapper();
+      mapper.getFactory().disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+
+      mapper.writeValue(writer, result);
+
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to write crawl results", e);
+    }
   }
 }
